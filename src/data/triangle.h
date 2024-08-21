74397af525ca471a01ee4e70b017135bf92ed501
Dennis Sedov <dennis@sedov.com>
2024-08-20 22:01:52 -0700

    normals are working finally

--- src/data/triangle.h
+++ src/data/triangle.h
@@ -13,17 +13,22 @@
 #include <pxr/usd/usd/stage.h>
 #include <pxr/usd/usdGeom/mesh.h>
 #include <pxr/base/vt/array.h>
+
 class triangle {
 public:
-    triangle(const point3& v0, const point3& v1, const point3& v2, shared_ptr<rd::core::material> mat)
-        : v0(v0), v1(v1), v2(v2), mat(mat) {
+    triangle(const point3& v0, const point3& v1, const point3& v2, 
+             const vec3& n0, const vec3& n1, const vec3& n2, 
+             shared_ptr<rd::core::material> mat)
+        : v0(v0), v1(v1), v2(v2), n0(n0), n1(n1), n2(n2), mat(mat) {
         edge1 = v1 - v0;
         edge2 = v2 - v0;
-        normal = unit_vector( cross(edge1, edge2) );
+        face_normal = unit_vector(cross(edge1, edge2));
     }
+
     aabb bounding_box() const {
         return aabb(v0, v1, v2);
     }
+
     bool hit(const ray& r, interval ray_t, hit_record& rec) const {
         const float EPSILON = 0.0000001;
         vec3 h = cross(r.direction(), edge2);
@@ -51,24 +56,33 @@ public:
         if (t > ray_t.min && t < ray_t.max) {
             rec.t = t;
             rec.p = r.at(t);
-            rec.set_face_normal(r, normal);
+            
+            // Interpolate normal
+            float w = 1 - u - v;
+            vec3 interpolated_normal = w * n0 + u * n1 + v * n2;
+            interpolated_normal = unit_vector(interpolated_normal);
+            
+            rec.set_face_normal(r, interpolated_normal);
             rec.mat = mat;
             return true;
         }
 
         return false;
     }
+
     void print() const {
         std::cout << "Triangle:\n"
-                  << "  v0: " << v0 << "\n"
-                  << "  v1: " << v1 << "\n"
-                  << "  v2: " << v2 << "\n"
-                  << "  normal: " << normal << "\n";
+                  << "  v0: " << v0 << ", n0: " << n0 << "\n"
+                  << "  v1: " << v1 << ", n1: " << n1 << "\n"
+                  << "  v2: " << v2 << ", n2: " << n2 << "\n"
+                  << "  face_normal: " << face_normal << "\n";
     }
 
 private:
     point3 v0, v1, v2;
-    vec3 edge1, edge2, normal;
+    vec3 n0, n1, n2;  // Vertex normals
+    vec3 edge1, edge2, face_normal;
     shared_ptr<rd::core::material> mat;
 };
+
 #endif
--- src/usd/geo.h
+++ src/usd/geo.h
@@ -31,13 +31,21 @@ namespace rd::usd::geo {
         pxr::VtArray<pxr::GfVec3f> points;
         pxr::VtArray<int> faceVertexCounts;
         pxr::VtArray<int> faceVertexIndices;
+        pxr::VtArray<pxr::GfVec3f> normals;
         std::vector<triangle> triangles;
 
         usd_mesh.GetPointsAttr().Get(&points);
         usd_mesh.GetFaceVertexCountsAttr().Get(&faceVertexCounts);
         usd_mesh.GetFaceVertexIndicesAttr().Get(&faceVertexIndices);
+        usd_mesh.GetNormalsAttr().Get(&normals);
+
+        // Print count for points, normals, and vertices
+        std::cout << "Number of points: " << points.size() << std::endl;
+        std::cout << "Number of normals: " << normals.size() << std::endl;
+        
 
         std::vector<point3> vertices;
+        std::vector<vec3> vertex_normals;
         point3 min_point(infinity, infinity, infinity);
         point3 max_point(-infinity, -infinity, -infinity);
 
@@ -53,6 +61,15 @@ namespace rd::usd::geo {
                 max_point[i] = std::max(max_point[i], vertex[i]);
             }
         }
+        std::cout << "Number of vertices: " << vertices.size() << std::endl;
+        // Transform normals
+        pxr::GfMatrix4d normalTransform = transform.GetInverse().GetTranspose();
+        for (const auto& normal : normals) {
+            pxr::GfVec3d transformedNormal = normalTransform.TransformDir(pxr::GfVec3d(normal));
+            vec3 normalizedNormal = unit_vector(vec3(transformedNormal[0], transformedNormal[1], transformedNormal[2]));
+            vertex_normals.push_back(normalizedNormal);
+        }
+
         size_t index = 0;
         for (int faceVertexCount : faceVertexCounts) {
             if (faceVertexCount < 3) {
@@ -74,7 +91,17 @@ namespace rd::usd::geo {
                     continue;
                 }
 
-                triangles.emplace_back(vertices[v0], vertices[v1], vertices[v2], mat);
+                if (index < vertex_normals.size() && index + i < vertex_normals.size() && index + i + 1< vertex_normals.size()) {
+                    triangles.emplace_back(vertices[v0], vertices[v1], vertices[v2],
+                                        vertex_normals[index], vertex_normals[index + i], vertex_normals[index + i + 1],
+                                        mat);
+                } else {
+                    // Use face normal as fallback for all vertices
+                    vec3 face_normal = unit_vector(cross(vertices[v1] - vertices[v0], vertices[v2] - vertices[v0]));
+                    triangles.emplace_back(vertices[v0], vertices[v1], vertices[v2],
+                                        face_normal, face_normal, face_normal,
+                                        mat);
+                }
             }
 
             index += faceVertexCount;
